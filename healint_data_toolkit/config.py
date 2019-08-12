0237eb148d52f3fe261dbe088b2237624a0f0421
Shadowsong27 <syk950527@gmail.com>
2019-08-12 22:29:40 +0800

    config parser init

---
+++ healint_data_toolkit/config.py
@@ -0,0 +1,48 @@
+import logging
+from typing import Dict
+
+import toml
+
+from healint_data_toolkit.exceptions import InvalidConfigFormatError
+from healint_data_toolkit.models.config import *
+from healint_data_toolkit.constants import ConfigKey
+
+
+class ConfigManager:
+
+    """
+        automatically construct config object for various purposes
+
+        requires the config file to have a standardized structure
+    """
+
+    def __init__(self, config_path: str):
+
+        with open(config_path, 'r') as file:
+            self.parsed_object = toml.loads(file.read())
+
+        self._db_config = {}
+
+    @property
+    def db_config(self) -> Dict[str, "DatabaseConfig"]:
+
+        if not self._db_config:
+            self._db_config = self._construct_db_config()
+
+        return self._db_config
+
+    def _construct_db_config(self) -> Dict[str, "DatabaseConfig"]:
+        if ConfigKey.DATABASE.value not in self.parsed_object:
+            raise InvalidConfigFormatError
+
+        database_object = self.parsed_object[ConfigKey.DATABASE.value]
+
+        db_config = {}
+
+        for credential_name, credential_info in database_object.items():
+            DatabaseConfig(**credential_info)
+            db_config[credential_name] = DatabaseConfig(**credential_info)
+
+        logging.warning(f"{len(db_config)} db credentials parsed. ")
+
+        return db_config
---
+++ healint_data_toolkit/exceptions.py
@@ -0,0 +1,19 @@
+class InvalidLoggingLevelError(Exception):
+    pass
+
+
+class InvalidConfigFormatError(Exception):
+    pass
+
+
+class UnknownLoadedQueryNameError(Exception):
+    pass
+
+
+class EmptyQueryError(Exception):
+    pass
+
+
+class DuplicateQueryNameError(Exception):
+    pass
+
